f4aa1f1c018a6bdb74d29f345577341a8db2a3bd
Daniel Braga <dbragabarbosa@gmail.com>
2023-01-10 10:50:55 -0300

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Daniel Braga Barbosa on 10/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

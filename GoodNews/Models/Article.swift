862358c1940f1661d05308541719915be3592d15
Daniel Braga <dbragabarbosa@gmail.com>
2023-01-18 10:38:02 -0300

    :construction: criando um modelo para o JSON

---
+++ GoodNews/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  GoodNews
+//
+//  Created by Daniel Braga Barbosa on 18/01/23.
+//
+
+import Foundation
+
+struct Article
+{
+    let title: String
+    let description: String
+}

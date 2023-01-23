6cb153befeb6efa28290c5820d0122e1ca82118a
Daniel Braga <dbragabarbosa@gmail.com>
2023-01-23 15:56:40 -0300

    :construction: Criando Cells e suas constraints

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Daniel Braga Barbosa on 23/01/23.
+//
+
+import Foundation
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
